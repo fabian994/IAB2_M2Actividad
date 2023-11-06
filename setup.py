b10b6df29af7095a8f04246c2b697f2239fbd15b
fabian994 <a01367585@itesm.mx>
2023-11-05 23:56:27 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import gdown
 from pyunpack import Archive
 import os
-link = 'https://drive.google.com/file/d/1wWgIbkw-aTU6h1i4Zh7E8EF-ySgJQe48/view?usp=sharing'#dataset
+link = 'https://drive.google.com/file/d/1L389q1jTTWNZ0dqvYXwyu8TuE6Gmg3Hq/view?usp=sharing'#dataset
 destination = 'finalDataset.zip'
 gdown.download(url=link, output=destination, quiet=False, fuzzy=True)
 Archive(destination).extractall('.')
